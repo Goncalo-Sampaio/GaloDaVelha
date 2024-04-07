8258c584f8ca8bd5b43bb3d1aa9777b6749f3b54
João Nogueira <joaonogueira.art@gmail.com>
2024-04-07 19:16:32 +0100

    Created name getter and add comments on it and to the Piece's constructor

--- GaloDaVelha/Piece.cs
+++ GaloDaVelha/Piece.cs
@@ -12,6 +12,27 @@ namespace GaloDaVelha
         private string symbol;
         private bool[] characteristics = new bool[4];
 
+        /// <summary>
+        /// This is the class Piece constructor
+        /// </summary>
+        /// <param name="name">
+        /// This is the Piece's name
+        /// </param>
+        /// <param name="symbol">
+        /// This is the Piece's symbol/symbols
+        /// </param>
+        /// <param name="square">
+        /// This says the Piece is a square(true) or a circle(false)
+        /// </param>
+        /// <param name="white">
+        /// This says the Piece is a white(true) or a black(false)
+        /// </param>
+        /// <param name="tall">
+        /// This says if the Piece is tall(true) or small(false)
+        /// </param>
+        /// <param name="hole">
+        /// This says if the Piece has a hole(true) or not(false)
+        /// </param>
         public Piece(string name, string symbol, bool square, bool white,
             bool tall, bool hole)
         {
@@ -21,6 +42,16 @@ namespace GaloDaVelha
             characteristics[1] = white;
             characteristics[2] = tall;
             characteristics[3] = hole;
+        }
+        /// <summary>
+        /// This a method to get the Piece's name
+        /// </summary>
+        /// <returns>
+        /// The Piece's name
+        /// </returns>
+        public string GetName()
+        {
+            return name;
         }
     }
 }
