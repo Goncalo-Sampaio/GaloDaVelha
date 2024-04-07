845abcd47624568c3ba549768c57e09c02c15631
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-04-07 19:46:16 +0100

    Added a for loop to check for a winning column on ConditionsChecker class

--- GaloDaVelha/ConditionsChecker.cs
+++ GaloDaVelha/ConditionsChecker.cs
@@ -54,6 +54,42 @@ namespace GaloDaVelha
                     }
                 }
 
+                //checks for a winning column
+
+                //resets similarityCounter to make a new check for column
+                similarityCounter = 0;
+
+                //checks for a winning column
+                for (int i = 0; i < board.GetLength(0); i++)
+                {
+                    //checks if there still an empty space on that column
+                    if (board[i, column] == null)
+                    {
+                        break;
+                    }
+
+                    //gets the piece on the position [i, column]
+                    Piece piece = board[i, column];
+
+                    //checks if the characteristic of the piece is true and adds
+                    //1 or false and subtracts 1
+                    if (piece.GetCharacteristics()[checkingCharacteristic])
+                    {
+                        similarityCounter++;
+                    }
+                    else
+                    {
+                        similarityCounter--;
+                    }
+
+                    //if 4 of the pieces have the same characteristic,
+                    //returns true and player wins
+                    if (similarityCounter == 4 || similarityCounter == -4)
+                    {
+                        return true;
+                    }
+                }
+
                 //increases the counter for which characteristic are we checking
                 checkingCharacteristic++;
             }
