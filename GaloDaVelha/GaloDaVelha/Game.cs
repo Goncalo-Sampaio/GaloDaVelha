6ff95bef04dbf0770f334fbc6271d4452b53faa4
João Nogueira <joaonogueira.art@gmail.com>
2024-04-07 18:47:09 +0100

    Merge

---
+++ GaloDaVelha/GaloDaVelha/Game.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GaloDaVelha
+{
+    public class Game
+    {
+        public void Start(){
+            Console.WriteLine("Game Started!");
+        }
+    }
+}
