4ced03a5bb1547ecdd59009abb3fa5ecb0a690eb
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-04-07 20:14:59 +0100

    Added a loop to check for the winning conditions

--- GaloDaVelha/Game.cs
+++ GaloDaVelha/Game.cs
@@ -5,10 +5,31 @@ using System.Threading.Tasks;
 
 namespace GaloDaVelha
 {
+    /// <summary>
+    /// Class where most of the game logic will run
+    /// </summary>
     public class Game
     {
+        private Piece[] availablePieces;
+        private Board board;
+        private Piece[,] spaces;
+        private bool gameEnded = false;
+        private int row;
+        private int column;
+        private ConditionsChecker checker = new ConditionsChecker();
+
+
         public void Start(){
-            Console.WriteLine("Game Started!");
+            board = new Board();
+            spaces = board.GetBoard();
+
+            //Main game loop that keeps going as long as there isn't a winner or
+            //a draw
+            while (!gameEnded)
+            {
+                gameEnded = checker.CheckWin(spaces, row, column);
+            }
+            Console.WriteLine("Game Over!");
         }
     }
 }
