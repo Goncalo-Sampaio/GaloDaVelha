10d8ad3edbcf6b55eb1410df6cf1e78375fbbd73
João Nogueira <joaonogueira.art@gmail.com>
2024-04-07 19:33:26 +0100

    Created Board's constructor and board getter and commented them

--- GaloDaVelha/Board.cs
+++ GaloDaVelha/Board.cs
@@ -5,8 +5,32 @@ using System.Threading.Tasks;
 
 namespace GaloDaVelha
 {
+    /// <summary>
+    /// This is the Board's class
+    /// </summary>
     public class Board
     {
-        
+        // Defining Piece's variables
+        private Piece[,] board;
+        private Piece[] pieces;
+
+        /// <summary>
+        /// This is the Board's constructor
+        /// </summary>
+        public Board()
+        {
+            board = new Piece[4, 4];
+        }
+
+        /// <summary>
+        /// This method gets the Board state
+        /// </summary>
+        /// <returns>
+        /// The Board state
+        /// </returns>
+        public Piece[,] GetBoard()
+        {
+            return board;
+        }
     }
 }
