1256894be61bb0418371391fa9759184955f0e20
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-04-07 19:38:57 +0100

    Added a for loop to check for a winning row on ConditionsChecker class

--- GaloDaVelha/ConditionsChecker.cs
+++ GaloDaVelha/ConditionsChecker.cs
@@ -23,6 +23,37 @@ namespace GaloDaVelha
                 //or diagonal have the same characteristic
                 int similarityCounter = 0;
 
+                //checks for a winning row
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    //checks if there still an empty space on that row
+                    if (board[row, j] == null)
+                    {
+                        break;
+                    }
+
+                    //gets the piece on the position [row, j]
+                    Piece piece = board[row, j];
+
+                    //checks if the characteristic of the piece is true and adds
+                    //1 or false and subtracts 1
+                    if (piece.GetCharacteristics()[checkingCharacteristic])
+                    {
+                        similarityCounter++;
+                    }
+                    else
+                    {
+                        similarityCounter--;
+                    }
+
+                    //if 4 of the pieces have the same characteristic,
+                    //returns true and player wins
+                    if (similarityCounter == 4 || similarityCounter == -4)
+                    {
+                        return true;
+                    }
+                }
+
                 //increases the counter for which characteristic are we checking
                 checkingCharacteristic++;
             }
