0b525d0810c2fc6b73688fc56dda255311d7b0d4
João Nogueira <joaonogueira.art@gmail.com>
2024-04-07 19:03:07 +0100

    Created Pieces constructor

--- GaloDaVelha/Piece.cs
+++ GaloDaVelha/Piece.cs
@@ -11,5 +11,16 @@ namespace GaloDaVelha
         private string name;
         private string symbol;
         private bool[] characteristics = new bool[4];
+
+        public Piece(string name, string symbol, bool square, bool white,
+            bool tall, bool hole)
+        {
+            this.name = name;
+            this.symbol = symbol;
+            characteristics[0] = square;
+            characteristics[1] = white;
+            characteristics[2] = tall;
+            characteristics[3] = hole;
+        }
     }
 }
