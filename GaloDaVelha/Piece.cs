927a4a1de10403b9cb73dd50fb68e77230cc8105
João Nogueira <joaonogueira.art@gmail.com>
2024-04-07 18:59:54 +0100

    Created Pieces class and variables

---
+++ GaloDaVelha/Piece.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GaloDaVelha
+{
+    public class Piece
+    {
+        // defining Piece's variables
+        private string name;
+        private string symbol;
+        private bool[] characteristics = new bool[4];
+    }
+}
