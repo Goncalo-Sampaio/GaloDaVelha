61eebffb62f09c16a0c7e3edaf3220d0080fd231
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-04-07 19:18:20 +0100

    Created ConditionsChecker class and method CheckWin

---
+++ GaloDaVelha/ConditionsChecker.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GaloDaVelha
+{
+    /// <summary>
+    /// This class will check the current conditions of the game to see if the
+    /// player that made the last move, won
+    /// </summary>
+    public class ConditionsChecker
+    {
+        public void CheckWin()
+        {
+            //which characteristic are we checking:
+            //0 for shape, 1 for color, 2 for size and 3 for hole
+            int checkingCharacteristic = 0;
+        }
+    }
+}
