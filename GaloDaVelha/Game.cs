30f104138b6c254a84df5a939ba201f35c63d8c4
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-04-07 20:25:48 +0100

    Added a method to show the game state to the user, calling it in the game loop

--- GaloDaVelha/Game.cs
+++ GaloDaVelha/Game.cs
@@ -11,25 +11,75 @@ namespace GaloDaVelha
     public class Game
     {
         private Piece[] availablePieces;
-        private Board board;
-        private Piece[,] spaces;
+        private Piece[,] board;
         private bool gameEnded = false;
+        private string player1;
+        private string player2;
         private int row;
         private int column;
         private ConditionsChecker checker = new ConditionsChecker();
 
 
         public void Start(){
-            board = new Board();
-            spaces = board.GetBoard();
+            //Instantiates a new Setup and gets the necessary variables
+            Setup setup = new Setup();
+            (availablePieces, board, player1, player2) = setup.GameSetup();
 
             //Main game loop that keeps going as long as there isn't a winner or
             //a draw
             while (!gameEnded)
             {
-                gameEnded = checker.CheckWin(spaces, row, column);
+                GameState();
+                gameEnded = checker.CheckWin(board, row, column);
             }
             Console.WriteLine("Game Over!");
+        }
+
+                /// <summary>
+        /// Shows the current game state to the user
+        /// </summary>
+        private void GameState()
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (j == board.GetLength(1) - 1)
+                    {
+                        if (board[i, j] == null)
+                        {
+                            Console.WriteLine("  ");
+                        }
+                        else
+                        {
+                            Console.WriteLine(board[i, j].GetSymbol());
+                        }
+                    }
+                    else
+                    {
+                        if (board[i, j] == null)
+                        {
+                            Console.Write("  |");
+                        }
+                        else
+                        {
+                            Console.Write($"{board[i, j].GetSymbol()}|");
+                        }
+                    }
+                }
+                if (i < board.GetLength(0) - 1)
+                {
+                    Console.WriteLine("-----------");
+                }
+            }
+            // Explaining symbols to the player
+            Console.WriteLine("Label:");
+            Console.WriteLine("\u25A1 = Big Squares"); // □
+            Console.WriteLine("\u25C7 = Small Squares"); // ◇
+            Console.WriteLine("\u25CB = Big Circles"); // ○
+            Console.WriteLine("\u25BD = Small Circles"); // ▽
+            Console.WriteLine("\u25E6 = Holes"); //◦
+            Console.WriteLine("- = No Holes"); //-
         }
     }
 }
